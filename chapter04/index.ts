5895d733bb2758b0813c3777594ad53b991e978a
kakakakakku <y.yoshida22@gmail.com>
2026-02-17 10:07:37 +0900

    Update Lambda Runtime from python3.12 to python3.13

--- chapter04/index.ts
+++ chapter04/index.ts
@@ -8,7 +8,7 @@ const chapter04 = new aws.iam.Role("chapter04", {
 new aws.lambda.Function("chapter04", {
     name: "chapter04-function",
     handler: "app.lambda_handler",
-    runtime: aws.lambda.Runtime.Python3d12,
+    runtime: aws.lambda.Runtime.Python3d13,
     role: chapter04.arn,
     code: new pulumi.asset.AssetArchive({
         ".": new pulumi.asset.FileArchive("./function/src"),
